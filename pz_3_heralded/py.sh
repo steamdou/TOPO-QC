984068cd40c963573819373c9b9f88ccb4f92444
Aubrey Zhang <aubreyz@uchicago.edu>
2026-02-08 02:41:38 -0600

    pz3 heralded

---
+++ pz_3_heralded/py.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=d4_sim_python          
+#SBATCH --account=pi-liangjiang
+#SBATCH --output=d4_sim-%J.out           
+#SBATCH --error=d4_sim-%J.err           
+#SBATCH --time=36:00:00                  
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32             
+#SBATCH --mem=64GB
+#SBATCH --partition=caslake
+
+# ---- Modules / environment ----
+module load python/anaconda-2022.05
+module load gurobi/11.0
+source activate /project/liangjiang/aubreyz/anaconda/topoqc
+# source activate pymatch
+
+# Avoid over-subscription from BLAS/MKL using too many threads
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+export TOPO_QC='/home/aubreyz/TOPO-QC'
+
+
+# ---- Go to the directory where you run sbatch ----
+cd "$TOPO_QC/pz_3_heralded"
+
+# If your main.py is in ./src/main.py:
+python -m src.main
