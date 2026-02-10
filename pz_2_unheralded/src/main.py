a8b632b980044dff792c307f89bf51ded4d143e9
Aubrey Zhang <aubreyz@uchicago.edu>
2026-02-10 12:43:57 -0600

    pz2 unheralded new

--- pz_2_unheralded/src/main.py
+++ pz_2_unheralded/src/main.py
@@ -73,8 +73,8 @@ if __name__ == "__main__":
     start_time = time.time()
 
     L = [4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7]
-    p = [0.16, 0.165, 0.17, 0.18, 0.19, 0.2]
-    stop = 2000
+    p = [0.152, 0.154, 0.156, 0.158, 0.16, 0.162, 0.164, 0.166]
+    stop = 10000
 
     error_rate = np.zeros((len(L), len(p)))
     counter = np.zeros((len(L), len(p)))
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
     # -------- Save to txt file --------
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    output_file = f"/project/liangjiang/aubreyz/run7_pz_2_unheralded/output_{timestamp}.txt"
+    output_file = f"/project/liangjiang/aubreyz/pz_2_unheralded_new/output_{timestamp}.txt"
 
     with open(output_file, "w") as f:
         f.write("Simulation parameters:\n")
