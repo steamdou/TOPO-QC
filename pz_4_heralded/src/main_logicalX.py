fb842fa22349f0ba884ae38fb72e9802625c9972
Aubrey Zhang <aubreyz@uchicago.edu>
2026-02-17 14:51:21 -0600

    paths edit

--- pz_4_heralded/src/main_logicalX.py
+++ pz_4_heralded/src/main_logicalX.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
 
     # -------- Save to txt file --------
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    output_file = f"/project/liangjiang/aubreyz/pz_4_heralded_X/output_{timestamp}.txt" # /Users/aubreyzhang/Documents
+    output_file = f"/project/liangjiang/aubreyz/pz_logicalXs/pz_4_heralded_X/output_{timestamp}.txt" # /Users/aubreyzhang/Documents
 
     with open(output_file, "w") as f:
         f.write("Simulation parameters:\n")
