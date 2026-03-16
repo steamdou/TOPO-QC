c8f6109e09084e5cfca7a55247e860bb543088a1
Aubrey Zhang <aubreyz@uchicago.edu>
2026-03-15 23:59:35 -0400

    debugging test

--- pz_4_heralded/src/main_Z_only.py
+++ pz_4_heralded/src/main_Z_only.py
@@ -31,7 +31,7 @@ def run_simulation(args):
     px = p[p_index]
     pz = 0.04
     w1 = np.log(1/px - 1)
-    w2 = np.log(1/pz - 1)
+    w2 = 0 #np.log(1/pz - 1)
     
     tot_count = 0
     error_count = 0
@@ -71,9 +71,10 @@ if __name__ == "__main__":
     start_time = time.time()
 
     L = [4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7]
-    p = [0.14, 0.142, 0.144, 0.146, 0.148, 0.15, 0.152, 0.144]
+    p = [0.152, 0.154, 0.156, 0.158, 0.16, 0.162, 0.164, 0.166]
+    #[0.14, 0.142, 0.144, 0.146, 0.148, 0.15, 0.152, 0.144]
     #[0.073, 0.083, 0.093, 0.103, 0.113, 0.123, 0.133, 0.143]
-    stop = 10000
+    stop = 8000
 
     error_rate = np.zeros((len(L), len(p)))
     counter = np.zeros((len(L), len(p)))
@@ -97,7 +98,7 @@ if __name__ == "__main__":
 
     # -------- Save to txt file --------
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    output_file = f"/project/liangjiang/aubreyz/pz_logicalZs/pz_4_heralded_Z/output_{timestamp}.txt"
+    output_file = f"/project/liangjiang/aubreyz/pz_logicalZs/pz_4_heralded_Z/test_mwpm_output_{timestamp}.txt"
 
     with open(output_file, "w") as f:
         f.write("Simulation parameters:\n")
