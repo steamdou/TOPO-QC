3f72c185e0b60a28b7f9caec88d6023b0991227d
Aubrey Zhang <aubreyz@uchicago.edu>
2026-05-01 00:59:57 -0500

    edit

--- pz_4_heralded/src/main_logicalX.py
+++ pz_4_heralded/src/main_logicalX.py
@@ -31,7 +31,7 @@ def run_simulation(args):
     px = p[p_index]
     pz = 0.04
     w1 = 1
-    w2 = 0.22
+    w2 = 0.4
     #0.08 worse
     #0.15
     
@@ -98,7 +98,7 @@ if __name__ == "__main__":
 
     # -------- Save to txt file --------
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    output_file = f"/project/liangjiang/aubreyz/pz_logicalXs/ILP_eff_X/pz_4_heralded_X/output0.22_{timestamp}.txt" # /Users/aubreyzhang/Documents
+    output_file = f"/project/liangjiang/aubreyz/pz_logicalXs/ILP_eff_X/pz_4_heralded_X/output0.4_{timestamp}.txt" # /Users/aubreyzhang/Documents
 
     with open(output_file, "w") as f:
         f.write("Simulation parameters:\n")
