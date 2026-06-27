a6eded253651151ce4c39696ef71c0f88cef97af
Aubrey Zhang <aubreyz@uchicago.edu>
2026-06-27 15:44:23 -0500

    conditional renamed

--- pz_2_heralded/pyZ_cond.sh
+++ pz_2_heralded/pyZ_cond.sh
@@ -26,7 +26,7 @@ export TOPO_QC='/home/aubreyz/TOPO-QC'
 
 
 # ---- Go to the directory where you run sbatch ----
-cd "$TOPO_QC/pz_5_heralded"
+cd "$TOPO_QC/pz_2_heralded"
 
 # If your main.py is in ./src/main.py:
-python -m src.main_logicalX_PE
+python -m src.main_logicalZ
