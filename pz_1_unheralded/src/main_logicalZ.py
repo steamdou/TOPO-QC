fa7596ac81be4ea919ed55b8169ae5e848b53ef4
Aubrey Zhang <aubreyz@uchicago.edu>
2026-07-12 13:16:52 -0400

    small test

--- pz_1_unheralded/src/main_logicalZ.py
+++ pz_1_unheralded/src/main_logicalZ.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 
     L = [4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7]
     p = [0.155, 0.157, 0.159, 0.161, 0.163, 0.165, 0.167, 0.169]
-    stop = 10000
+    stop = 10
 
     error_rate = np.zeros((len(L), len(p)))
     counter = np.zeros((len(L), len(p)))
