da9626f5bedf08e4dbfcf3b84e38301be0c232d5
Aubrey Zhang <aubreyz@uchicago.edu>
2026-07-13 23:17:51 -0400

    file name change

--- pz_1_unheralded/src/main_logicalZ.py
+++ pz_1_unheralded/src/main_logicalZ.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 
     # -------- Save to txt file --------
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    output_file = f"/project/verresen/aubreyz/logicalZs/MWPMZ/pz_1_unheralded_Z/TEST_output_{timestamp}.txt"
+    output_file = f"/project/verresen/aubreyz/logicalZs/MWPMZ/pz_1_unheralded_Z/CORRECTED_output_{timestamp}.txt"
 
     with open(output_file, "w") as f:
         f.write("Simulation parameters:\n")
