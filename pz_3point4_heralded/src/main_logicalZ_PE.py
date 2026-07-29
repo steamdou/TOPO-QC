7cc584ea0e7b68171d568aa8ec42a074af604592
Aubrey Zhang <aubreyz@uchicago.edu>
2026-07-29 16:23:27 -0500

    pz 3.4

--- pz_3point4_heralded/src/main_logicalZ_PE.py
+++ pz_3point4_heralded/src/main_logicalZ_PE.py
@@ -29,36 +29,41 @@ def run_simulation(args):
 
     l_index, p_index, L, p, stop = args
     px = p[p_index]
-    pz = 0.033
+    pz = 0.034
     w1 = np.log(1/px - 1)
     w2 = np.log(1/pz - 1)
-    #0.4
+    #0.3
+    #0.57
     
     tot_count = 0
     error_count = 0
+    # odd_count = 0
     cn_dict = connection_dict((L[l_index]*3,L[l_index]*3))
     V, E1_list, E2_list, Gamma1, Gamma2, w1_arr, w2_arr = build_ILP_structure((L[l_index]*3,L[l_index]*3), cn_dict, w1, w2)
 
     while tot_count < stop:
         tot_count += 1
-        code = D4_Code(L[l_index], np.array([0,1,2]), cn_dict, V, E1_list, E2_list, Gamma1, Gamma2, w1_arr.copy(), w2_arr.copy(), env=_worker_env, rng=_worker_rng)
+        code = D4_Code(L[l_index], np.array([]), cn_dict, V, E1_list, E2_list, Gamma1, Gamma2, w1_arr.copy(), w2_arr.copy(), env=_worker_env, rng=_worker_rng)
         code.X_errors(px)
         code.Z_errors(pz)
         s = code.measure_e_anyons()
         if (np.isscalar(s) and s == 5):
-            error_count += 1
+            raise ValueError('collapsed X logical')
         else:
             code.flux_correction()
             output = code.correct_e_anyons()
-            if (np.isscalar(output) and output == 5):
+            if output == 3:
+                # return 3 if there are odd number of e-anyons for any color
+                # odd_count += 1
                 error_count += 1
+            elif output == 5:
+                raise ValueError('collapsed X logical')
+            elif output == 0:
+                if not np.array_equal(code.LZ, np.zeros(12)):
+                    error_count += 1
             else:
-                lx_out = code.decode_X_logicals()
-                if lx_out==5:
-                    error_count += 1
-                elif lx_out:
-                    error_count += 1
- 
+                raise ValueError('unexpected output')
+    # print(odd_count, error_count)
     error_rate = error_count / tot_count
     return l_index, p_index, error_rate, tot_count
 
@@ -68,10 +73,10 @@ if __name__ == "__main__":
     start_time = time.time()
 
     L = [4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7]
-    p = [0.143, 0.145, 0.147, 0.149, 0.151, 0.153, 0.155, 0.157]
+    p = [0.141, 0.143, 0.145, 0.147, 0.149, 0.151, 0.153, 0.155]
     #[0.152, 0.154, 0.156, 0.158, 0.16, 0.162, 0.164, 0.166]
-    #[0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2]
-    #[0.155, 0.157, 0.159, 0.161, 0.163, 0.165, 0.167, 0.169]
+    #[0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20, 0.21]
+    #[0.157, 0.159, 0.161, 0.163, 0.165, 0.167, 0.169, 0.171]
     stop = 8000
 
     error_rate = np.zeros((len(L), len(p)))
@@ -96,7 +101,7 @@ if __name__ == "__main__":
 
     # -------- Save to txt file --------
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    output_file = f"/project/verresen/aubreyz/logicalXs/ILP_PE_X/pz_3point3_heralded_X/output_{timestamp}.txt" # /Users/aubreyzhang/Documents
+    output_file = f"/project/verresen/aubreyz/logicalZs/ILP_PE_Z/pz_3point4_heralded_Z/output_{timestamp}.txt"
 
     with open(output_file, "w") as f:
         f.write("Simulation parameters:\n")
